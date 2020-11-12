fb16032b5559998b5af46b0c1afe274529bb272d
Coospir <eugene.starodubov@gmail.com>
2020-11-12 17:50:28 +0300

    Basic components..

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="header__block">
+            <a href="#1">Home</a>
+            <a href="#2">News</a>
+            <a href="#3">Contacts</a>
+        </div>
+    );
+}
+
+export default Header;
