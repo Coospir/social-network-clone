027e81c963777b66bc906ce65985226d210687c9
Coospir <eugene.starodubov@gmail.com>
2020-12-09 10:43:55 +0300

    Updated footer

--- src/blocks/Header.js
+++ src/blocks/Header.js
@@ -1,10 +1,12 @@
 const Header = () => {
     return (
         <div className="header__block">
-            <img src='https://cdn.logo.com/hotlink-ok/logo-social-sq.png' alt='' />
-            <a className="header__item" href="#1">Profile</a>
-            <a className="header__item" href="#2">News</a>
-            <a className="header__item" href="#3">Settings</a>
+            <div className="main__menu">
+                <img src='https://cdn.logo.com/hotlink-ok/logo-social-sq.png' alt='' />
+                <a className="header__item" href="#1">Profile</a>
+                <a className="header__item" href="#2">News</a>
+                <a className="header__item" href="#3">Settings</a>
+            </div>
             <div className="user__block">
                 <a className="user__item" href="#1">Login</a>
                 <a className="user__item" href="#2">Signup</a>
